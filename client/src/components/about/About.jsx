67420bca40b4841fac0997a9ea8c1dc53d2aedb9
iazagi <101048623+iazagi@users.noreply.github.com>
2023-03-01 14:13:52 +0200

    experience tab

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import './about.css';
 import ME from '../../assets/me-about.jpg';
+import { FaAward, FaUsers, FaFolder } from 'react-icons/fa';
 
 const About = () => {
   return (
@@ -8,12 +10,41 @@ const About = () => {
       <h2>ABOUT ME</h2>
       <div className="container about__container">
         <div className="about__me">
-          <div className="about_me-image">
+          <div className="about__me-image">
             <img src={ME} alt="img" />
           </div>
         </div>
 
-        <div className="about__content"></div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ Year </small>
+            </article>
+
+            <article className="about__card">
+              <FaUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>0 </small>
+            </article>
+
+            <article className="about__card">
+              <FaFolder className="about__icon" />
+              <h5>Projects</h5>
+              <small>10 complited</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+            saepe animi aut exercitationem, harum aspernatur nulla ipsam nobis
+            pariatur dicta culpa similique, vel porro illum inventore amet earum
+            accusantium eaque.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
       </div>
     </section>
   );
--- client/src/components/experience/Experience.jsx
+++ client/src/components/experience/Experience.jsx
@@ -1,8 +1,75 @@
 import React from 'react';
 import './experience.css';
+import { GrHtml5 } from 'react-icons/gr';
+import { IoLogoCss3 } from 'react-icons/io';
+import { SiJavascript, SiMongodb, SiPostgresql } from 'react-icons/si';
+import { TbBrandReactNative } from 'react-icons/tb';
+import { GrReactjs } from 'react-icons/gr';
+import { TbBrandTailwind } from 'react-icons/tb';
+import { BsBootstrapFill } from 'react-icons/bs';
+import { FaNodeJs } from 'react-icons/fa';
 
 const experience = () => {
-  return <div></div>;
+  return (
+    <section className="" id="experience">
+      <h5>what skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <GrHtml5 className="experience__details-icon" />
+              <h4>HTML</h4>
+            </article>
+            <article className="experience__details">
+              <IoLogoCss3 className="experience__details-icon" />
+              <h4>CSS</h4>
+            </article>
+            <article className="experience__details">
+              <SiJavascript className="experience__details-icon" />
+              <h4>JAVASCRIPT</h4>
+            </article>
+            <article className="experience__details">
+              <GrReactjs className="experience__details-icon" />
+              <h4>REACT</h4>
+            </article>
+            <article className="experience__details">
+              <TbBrandReactNative className="experience__details-icon" />
+              <h4>REACT NATIVE</h4>
+            </article>
+            <article className="experience__details">
+              <TbBrandTailwind className="experience__details-icon" />
+              <h4>TAILWIND</h4>
+            </article>
+            <article className="experience__details">
+              <BsBootstrapFill className="experience__details-icon" />
+              <h4>BOOTSTRAP</h4>
+            </article>
+          </div>
+        </div>
+        {/*end of frontend */}
+        <div className="experience__backend">
+          <h3>backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaNodeJs className="experience__details-icon" />
+              <h4>NODE-JS</h4>
+            </article>
+            <article className="experience__details">
+              <SiMongodb className="experience__details-icon" />
+              <h4>MANGODB</h4>
+            </article>
+            <article className="experience__details">
+              <SiPostgresql className="experience__details-icon" />
+              <h4>POSTGRESQL</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default experience;
